13c96c5f6f7780769c2720bc260353f349cad3e1
randy <1287530097@qq.com>
2021-12-09 15:56:25 +0800

    js

--- js/boolean.js
+++
@@ -1,7 +0,0 @@
-const bool1 = true;
-console.log(bool1); // true
-const bool2 = new Boolean(1);
-console.log(bool2); // [Boolean: true]
-console.log(bool2.toString(), bool2.valueOf()); // 'true' true
-const bool3 = Boolean(1);
-console.log(bool3); // true
